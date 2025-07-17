aaf10101375457c836fe8086fe56658842b6a720
Maxence Osmond <mosmond@c1r6p2.42lehavre.fr>
2025-07-17 18:58:57 +0200

    need more work

--- C03r/ex04/ft_strstr.c
+++ C03r/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mosmond <mosmond@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 09:46:33 by mosmond           #+#    #+#             */
-/*   Updated: 2025/07/15 17:59:22 by mosmond          ###   ########.fr       */
+/*   Updated: 2025/07/17 18:58:11 by mosmond          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,27 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
+	if (!(to_find[0]))
+		return (&str[0]);
 	while (str[i])
 	{
 		if (str[i] == to_find[0])
 		{
 			j = 1;
 			while (to_find[j] && str[i + j] == to_find[j])
-			{
 				j++;
-			}
-			if (to_find[j] == 0)
-			{
+			if (!to_find[j])
 				return (&str[i]);
-			}
 		}
 		i++;
 	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%s\n",ft_strstr("abcdefghijklmnopqrstuvwxyz", "m"));
+	printf("%s\n",ft_strstr("abcdefghijklmnopqrstuvwxyz", "9"));
+	printf("%s\n",ft_strstr("abcdefghijklmnopqrstuvwxyz", "\0"));
 	return (0);
 }
