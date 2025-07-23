42cc13e23b251f98535963cf76fee217faf1c095
Maxence Osmond <mosmond@c1r4p11.42lehavre.fr>
2025-07-23 09:01:34 +0200

    50% C08

--- C08/ex00/ft.h
+++ C08/ex00/ft.h
@@ -6,17 +6,17 @@
 /*   By: mosmond <mosmond@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:05:33 by mosmond           #+#    #+#             */
-/*   Updated: 2025/07/22 18:03:23 by mosmond          ###   ########.fr       */
+/*   Updated: 2025/07/23 08:48:09 by mosmond          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
--- C08/ex03/ft_point.h
+++ C08/ex03/ft_point.h
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mosmond <mosmond@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 14:15:46 by mosmond           #+#    #+#             */
-/*   Updated: 2025/07/23 08:36:46 by mosmond          ###   ########.fr       */
+/*   Created: 2025/07/23 08:51:17 by mosmond           #+#    #+#             */
+/*   Updated: 2025/07/23 08:56:54 by mosmond          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN_H
-# define FT_BOOLEAN_H
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
-# define TRUE 1
-# define FALSE 0
-# define EVEN(n) (1 - (n % 2))
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-# define SUCCESS 0
-
-typedef int	t_bool;
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
 
 #endif
